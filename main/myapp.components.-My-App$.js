e34ce6084433efbf4314ed24f09bcd49e0ac2a13
nguyenyou <tunt081295@gmail.com>
2025-11-20 10:46:56 +0700

    🚢 ship it

--- main/myapp.components.-My-App$.js
+++ main/myapp.components.-My-App$.js
@@ -3,6 +3,7 @@ import * as $j_internal$002d3ebfae0cba70adf981029a0da5b1e4b5ab5d02c6 from "./int
 import * as $j_myapp$002e$002dChild$002dModifier from "./myapp.-Child-Modifier.js";
 import * as $j_myapp$002e$002dFunction$002dComponent from "./myapp.-Function-Component.js";
 import * as $j_myapp$002e$002dModifier from "./myapp.-Modifier.js";
+import * as $j_myapp$002e$002dModifier$0024 from "./myapp.-Modifier$.js";
 import * as $j_myapp$002e$002dProp$002dModifier from "./myapp.-Prop-Modifier.js";
 import * as $j_myapp$002e$002dStyle$002dModifier from "./myapp.-Style-Modifier.js";
 import * as $j_myapp$002ecomponents$002e$002dMy$002dApp from "./myapp.components.-My-App.js";
@@ -27,7 +28,7 @@ function $c_Lmyapp_components_MyApp$() {
     var $x_3 = new $j_myapp$002e$002dStyle$002dModifier.$c_Lmyapp_StyleModifier("color", "#000");
     var $x_2 = new $j_myapp$002e$002dStyle$002dModifier.$c_Lmyapp_StyleModifier("height", "200px");
     var $x_1 = new $j_myapp$002e$002dStyle$002dModifier.$c_Lmyapp_StyleModifier("border", "1px solid #000");
-    var element = $j_myapp$002ehtml$0024.$m_Lmyapp_html$().div__sci_Seq__Lmyapp_ReactElement($j_internal$002d3ebfae0cba70adf981029a0da5b1e4b5ab5d02c6.$m_sr_ScalaRunTime$().wrapRefArray__AO__sci_ArraySeq(new ($j_myapp$002e$002dModifier.$d_Lmyapp_Modifier.getArrayOf().constr)([])));
+    var element = $j_myapp$002ehtml$0024.$m_Lmyapp_html$().div__sci_Seq__Lmyapp_ReactElement($j_internal$002d3ebfae0cba70adf981029a0da5b1e4b5ab5d02c6.$m_sr_ScalaRunTime$().wrapRefArray__AO__sci_ArraySeq(new ($j_myapp$002e$002dModifier.$d_Lmyapp_Modifier.getArrayOf().constr)([$j_myapp$002e$002dModifier$0024.$m_Lmyapp_Modifier$().stringToModifier__T__Lmyapp_Modifier("Hello world")])));
     return $x_8.div__sci_Seq__Lmyapp_ReactElement($x_7.wrapRefArray__AO__sci_ArraySeq(new ($j_myapp$002e$002dModifier.$d_Lmyapp_Modifier.getArrayOf().constr)([$x_6, $x_5, $x_4, $x_3, $x_2, $x_1, new $j_myapp$002e$002dChild$002dModifier.$c_Lmyapp_ChildModifier(element)])));
   }));
   this.Lmyapp_components_MyApp$__f_component = new $j_myapp$002e$002dFunction$002dComponent.$c_Lmyapp_FunctionComponent("MyApp", render);
--- src/components/MyApp.scala
+++ src/components/MyApp.scala
@@ -6,8 +6,7 @@ import myapp.React
 import myapp.ReactElement
 import myapp.tags.button
 import myapp.tags.h3
-import myapp.tags.div
-import myapp.html
+import myapp.html.div
 import myapp.components.ThemeContext
 import myapp.components.Greeting
 import myapp.components.Counter
@@ -37,14 +36,14 @@ object MyApp {
   })
 
   val component = FunctionComponent[Props]("MyApp") { _ =>
-    html.div(
+    div(
       onClick := handleClickContainer,
       marginLeft := "20px",
       background := "#fff",
       color := "#000",
       height := "200px",
       border := "1px solid #000",
-      html.div()
+      div("Hello world")
     )
   }
 
